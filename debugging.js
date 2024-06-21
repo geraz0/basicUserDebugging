f987cb323781c38619791c2a9ab4771efce03acc
geraz0 <gabbi.erazo1@gmail.com>
2024-06-21 14:33:24 -0400

    debugged code

--- debugging.js
+++ debugging.js
@@ -1,16 +1,16 @@
 const storeOwners = [
     {
-      name: 'Danny Shavez',
+      name: 'charles',
       stores: 1,
       location: "AZ",
     },
     {
-      name: 'Danny Shavez',
+      name: 'sally',
       stores: 1,
       location: "NM",
     },
     {
-      name: 'Danny Shavez',
+      name: 'cassandra',
       stores: 1,
       location: "NM",
     },
@@ -23,28 +23,35 @@ const storeOwners = [
   
   const listNumberOfStores = function () {
     for (let i = 0; i < storeOwners.length; i++) {
-      let totalLocations = totalLocaions + storeOwners.stores;
+      let totalLocations = totalLocations + storeOwners[i].stores;
     }
     return i;
   };
   
   let locations = listNumberOfStores;
+  //let totalLocations = 0;
+  
   
   function tellMeMyStores() {
     console.log('Hey, can you tell me how many stores you have?');
     if (locations > 0) {
-      console.log('Of course, we have ' + locations + ' stores');
+      console.log('Yes, we have ' + locations + ' stores');
     }
   }
   
   function hasStore() {
     for (let i = 0; i < storeOwners.length; i++) {
-      let person = storeOwners.name;
-      let location = storeOwners.location();
-      console.log('Yes, {person} has one in {this.location}');
+      let person = storeOwners[i].name;
+      let location = storeOwners[i].location;
+      console.log(`Yes, ${person} has one in ${location}`);
     }
   
   }
-  
-  tellMeMyStores();
-  hasStore();
+
+
+tellMeMyStores();
+hasStore();
+
+// let array = [1, 2, 3, 4, 5, 6,];
+//  array[1]
+//  console.log(array[1]);
